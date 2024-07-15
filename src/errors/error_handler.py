d085cc6d4b062640934c1afefa746a51e34d828f
Gabriel Alvarenga <gabriel_dsalvarenga@hotmail.com>
2024-07-15 18:27:02 -0300

    feat: implementing error handler

---
+++ src/errors/error_handler.py
@@ -0,0 +1,9 @@
+from src.views.http_types.http_response import HttpResponse
+
+def handle_errors(error: Exception) -> HttpResponse:
+    return HttpResponse(
+        status_code=500, 
+        body={'error': [{
+        'title': 'Internal Server Error',
+        'detail': str(error)
+        }]})
--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,9 +1,13 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.controllers.tag_creator_controller import TagCreatorController
 class TagCreatorView:
 
     def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
-        # body = http_request.body
-        # product_code = body['product_code']
+        tag_creator_controller = TagCreatorController()
+        body = http_request.body
+        product_code = body['product_code']
 
-        return HttpResponse(status_code=200, body={'message': 'Tag created!'})
+        formatted_response = tag_creator_controller.create(product_code)
+
+        return HttpResponse(status_code=200, body=formatted_response)

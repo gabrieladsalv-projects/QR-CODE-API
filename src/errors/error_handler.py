27cdf36fd74a6758afd97e3d4813fe831bf53bdd
Gabriel Alvarenga <gabriel_dsalvarenga@hotmail.com>
2024-07-16 13:36:02 -0300

    feat: implementing validator

--- src/errors/error_handler.py
+++ src/errors/error_handler.py
@@ -1,9 +1,19 @@
 from src.views.http_types.http_response import HttpResponse
+from src.errors.error_types.http_unprocessable_entity_error import HttpUnprocessableEntityError
 
 def handle_errors(error: Exception) -> HttpResponse:
+    if isinstance(error, HttpUnprocessableEntityError):
+        return HttpResponse(
+            status_code=error.status_code, 
+            body={'errors': [{
+                'title': error.name,
+                'detail': error.message
+
+            }]})
+
     return HttpResponse(
         status_code=500, 
         body={'error': [{
-        'title': 'Internal Server Error',
+        'title': 'Server Error',
         'detail': str(error)
         }]})
